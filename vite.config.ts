0dcbbf1ee3d9a7cbe28fd98e985dff1cc146e20b
tiouoo <tiouo@qq.com>
2026-04-26 00:51:40 +0800

    feat: 添加PWA支持及相关配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import pwaConfig from './pwa.config.js';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), VitePWA(pwaConfig)],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
